4041ce5342e045ca49d4a4d0080eb6360166b0ae
Jesko2004 <284599280@qq.com>
2026-03-26 19:00:44 +0800

    新增了一行时间代码

--- __pycache__/asyncio_demo.py
+++ __pycache__/asyncio_demo.py
@@ -53,6 +53,7 @@ async def main():
 asyncio.run(main())
 """
 
+"""
 class Rock:
     def __await__(self):
         value_sent_in = yield 7
@@ -77,3 +78,21 @@ try:
 except StopIteration as e:
     returned_value = e.value
 print(f"Coroutine main() finished and provided value: {returned_value}.")
+"""
+
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+async def main():
+    print(f"started at {time.strftime('%X')}")
+
+    await say_after(1, 'hello')
+    await say_after(2, 'world')
+
+    print(f"finished at {time.strftime('%X')}")
+
+asyncio.run(main())
