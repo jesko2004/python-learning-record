aee174c3181c2dfe2341f58f41692dda14be14e9
Jesko2004 <284599280@qq.com>
2026-03-26 18:43:46 +0800

    asyncio学习完

---
+++ __pycache__/asyncio_demo.py
@@ -0,0 +1,79 @@
+"""
+import asyncio
+async def loudmouth_penguin(magic_number: int):
+    print(
+     "I am a super special talking penguin. Far cooler than that printer. "
+     f"By the way, my lucky number is: {magic_number}."
+    )
+
+
+async def main():
+    await loudmouth_penguin(42)
+
+if __name__ == "__main__":
+    asyncio.run(main())
+"""
+
+
+"""import asyncio
+
+async def coro_a():
+   print("I am coro_a(). Hi!")
+
+async def coro_b():
+   print("I am coro_b(). I sure hope no one hogs the event loop...")
+
+async def main():
+   num_repeats = 3
+   for _ in range(num_repeats):
+      await coro_a()
+      task_b = asyncio.create_task(coro_b())
+      await task_b
+
+asyncio.run(main())
+"""
+
+
+"""
+import asyncio
+
+async def coro_a():
+   print("I am coro_a(). Hi!")
+
+async def coro_b():
+   print("I am coro_b(). I sure hope no one hogs the event loop...")
+
+async def main():
+   task_b = asyncio.create_task(coro_b())
+   num_repeats = 3
+   for _ in range(num_repeats):
+       await asyncio.create_task(coro_a())
+   await task_b
+
+asyncio.run(main())
+"""
+
+class Rock:
+    def __await__(self):
+        value_sent_in = yield 7
+        print(f"Rock.__await__ resuming with value: {value_sent_in}.")
+        return value_sent_in
+
+async def main():
+    print("Beginning coroutine main().")
+    rock = Rock()
+    print("Awaiting rock...")
+    value_from_rock = await rock
+    print(f"Coroutine received value: {value_from_rock} from rock.")
+    return 23
+
+coroutine = main()
+intermediate_result = coroutine.send(None)
+print(f"Coroutine paused and returned intermediate value: {intermediate_result}.")
+
+print(f"Resuming coroutine and sending in value: 42.")
+try:
+    coroutine.send(42)
+except StopIteration as e:
+    returned_value = e.value
+print(f"Coroutine main() finished and provided value: {returned_value}.")
