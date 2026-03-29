47d4d0a8fa422094a95f5132819321ff8b64ca8e
Jesko2004 <284599280@qq.com>
2026-03-29 14:49:59 +0800

    pytest前置学完

---
+++ pytest.py
@@ -0,0 +1,37 @@
+import pytest
+
+"""
+
+def inc(x):
+    return x + 1
+
+
+def test_answer():
+    assert inc(3) == 4 #断言assert
+
+ """
+
+
+"""
+@pytest.fixture
+
+def input_value():
+
+   input = 39
+
+   return input#模拟fixture
+
+
+
+def test_divisible_by_3(input_value):
+
+   assert input_value % 3 == 0
+
+
+
+def test_divisible_by_6(input_value):
+
+   assert input_value % 6 == 0
+
+"""
+
